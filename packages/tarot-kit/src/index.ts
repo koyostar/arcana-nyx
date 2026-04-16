f4bd0c00a37ac16ffd365c20a22a813108a41ed4
cometpisces <shermaine.lsh@gmail.com>
2026-04-16 17:10:40 +0800

    Define public API exports
    - Export all core types and interfaces
    - Export card data and helper functions
    - Provide clean public API surface for package consumers

--- packages/tarot-kit/src/index.ts
+++ packages/tarot-kit/src/index.ts
@@ -1,3 +1,20 @@
-export const helloTarot = (): string => {
-  return "Tarot kit is alive";
-};
+export type {
+  ArcanaType,
+  MinorArcanaSuit,
+  CardOrientation,
+  TarotCardMeaning,
+  TarotCard,
+  DrawnCard,
+} from "./types/card.js";
+
+export { cards } from "./data/cards.js";
+export {
+  getAllCards,
+  getCardById,
+  getCardsByArcana,
+} from "./helpers/cards.js";
+export { drawRandomCard, drawCards, getCardMeaning } from "./helpers/draw.js";
+export { validateUniqueCardIds } from "./helpers/validate.js";
+
+export type { SupportedLanguage, LocalizedText } from "./types/common.js";
+export { getLocalizedText } from "./helpers/localize.js";
